4b84668a11fc6e99279aaafbb2caf9d2b6bab5fa
isaacgrove <isaacgrove333@gmail.com>
2020-12-10 13:20:10 -0500

    day 8 unexplained

---
+++ day_8/day_8.py
@@ -0,0 +1,73 @@
+def handle_input(file):
+    '''Accepts source text from problem input webpage 
+    (saved as .txt file), returns list of boot code instructions'''
+    inp = open(file)
+    lst = inp.readlines()
+    for i in range(len(lst)):
+        lst[i] = lst[i].rstrip('\n')
+    #lst = my_str.split('\n\n')
+    return lst
+
+lst = (handle_input('input.txt'))
+print(f'There are {len(lst)} instructions in the input (including for reference).')
+
+
+# Part 1
+
+def pt1_accumulator_before_loop():
+    accumulator = 0
+
+    visited = [False]*len(lst)
+    idx = 0
+    while visited[idx] == False:
+        visited[idx] = True
+        if lst[idx][:3] == 'acc':
+            accumulator += int(lst[idx][4:])
+            idx += 1
+        elif lst[idx][:3] == 'jmp':
+            idx += int(lst[idx][4:])
+        else:
+            idx += 1
+    print(f'Part 1: the accumulator is {accumulator} before the loop.')
+
+pt1_accumulator_before_loop()
+
+# Part 2
+
+def run_boot_code(lst, end_found):
+    visited = [False]*len(lst)
+    accumulator = 0
+    idx = 0
+    while visited[idx] == False:
+        visited[idx] = True
+        if lst[idx][:3] == 'acc':
+            accumulator += int(lst[idx][4:])
+            idx += 1
+        elif lst[idx][:3] == 'jmp':
+            idx += int(lst[idx][4:])
+        else:
+            idx += 1
+        if idx == len(lst):
+            print('Part 2: accumulator = ', accumulator, 'upon boot code termination.')
+            end_found.append('hi')
+            break
+
+def pt2_accumulator_after_termination():
+    idx_replaced = 0
+    end_found = []
+    acc = 0
+    while len(end_found) == 0:
+        lst2 = lst.copy()
+        for i in range(idx_replaced + 1, len(lst)):
+            if lst2[i][:3] == 'jmp':
+                lst2[i] = lst2[i].replace('jmp', 'nop')
+                idx_replaced = i
+                break
+            if lst2[i][:3] == 'nop':
+                lst2[i] = lst2[i].replace('nop', 'jmp')
+                idx_replaced = i
+                break
+        run_boot_code(lst2, end_found)
+        acc += 1
+
+pt2_accumulator_after_termination()
